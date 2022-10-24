79df4188ccadaf745ba1f89376594d654bb2c030
jamie082 <jamiemorrissey02@gmail.com>
2022-10-23 19:39:16 -0700

    added source

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from tkinter import messagebox
 
 win = Tk()
 
-win.geometry("700x350")
-win.title("Python Guide")
+win.geometry("700x150")
+win.title("Web Scraper Site")
 
-scrape_site = "http://45.77.988.77/site/temp_site/main.html"
+scrape_site = "http://45.77.98.77/site/temp_site/main.html"
+frame_site = "http://45.77.98.57/site/temp_site/frame_1.html"
 
 r = Label(win, text="http://45.77.988.77/site/temp_site/main.html", width=35)
 r.pack()
