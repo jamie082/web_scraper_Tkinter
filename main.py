83d312eab6f46e4748d61fe42d3742f7d86a6ecb
jamie082 <jamiemorrissey02@gmail.com>
2022-10-23 21:38:21 -0700

    added button widget

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ win = Tk()
 win.geometry("700x650")
 win.title("Web Scraper Site")
 
-
 scrape_site = "http://45.77.98.77/site/temp_site/main.html"
 frame_site = "http://45.77.98.57/site/temp_site/frame_1.html"
 
 r = Label(win, text="http://45.77.988.77/site/temp_site/main.html", width=35)
+
+click_button = Button(win, text="Scrape")
+
 
 text_box = Text(
     win,
@@ -19,10 +21,14 @@ text_box = Text(
 )
 r.pack()
 
+click_button.pack()
+
 message=''
 text_box.pack(expand=True)
 text_box.insert('end', message)
 text_box.config(state='disabled')
+
+
 
 win.resizable(False, False)
 win.mainloop()
