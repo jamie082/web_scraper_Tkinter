425b3c22c802684df08d806098738864a14bb829
jamie082 <jamiemorrissey02@gmail.com>
2022-10-23 19:31:13 -0700

    added resizable

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ win = Tk()
 win.geometry("700x350")
 win.title("Python Guide")
 
+scrape_site = "http://45.77.988.77/site/temp_site/main.html"
+
+r = Label(win, text="http://45.77.988.77/site/temp_site/main.html", width=35)
+r.pack()
+
+win.resizable(False, False)
 win.mainloop()
