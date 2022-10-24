fd2e97a9f7a4b6c792fe599ef27dcba84753a5a5
jamie082 <jamiemorrissey02@gmail.com>
2022-10-24 00:36:55 -0700

    added GUI fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 win = Tk()
 
-win.geometry("700x650")
+win.geometry("700x300")
 win.title("Web Scraper Site")
 
 scrape_site = "http://45.77.98.77/site/temp_site/main.html"
@@ -17,10 +17,11 @@ page = requests.get(frame_site)
 r = Label(win, text="http://45.77.988.77/site/temp_site/main.html", width=35)
 
 def scraper():
+    # scrape to inside text box
     return page.content
 
 def my_function():  # start Web Scraper code
-    messagebox.showinfo("Alert", "Executed Web Scraper")
+    messagebox.showinfo("Program Alert", "Web Scraper Started")
     text_box.insert(END, scraper)
     print(page.content) # put it inside text_box
 
