5f729ab876e921d16a0becd1952d67bb97dd1087
jamie082 <jamiemorrissey02@gmail.com>
2022-10-23 21:26:36 -0700

    added text_box

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ from tkinter import messagebox
 
 win = Tk()
 
-win.geometry("700x150")
+win.geometry("700x650")
 win.title("Web Scraper Site")
+
 
 scrape_site = "http://45.77.98.77/site/temp_site/main.html"
 frame_site = "http://45.77.98.57/site/temp_site/frame_1.html"
 
 r = Label(win, text="http://45.77.988.77/site/temp_site/main.html", width=35)
+
+text_box = Text(
+    win,
+    height=12,
+    width=80
+)
 r.pack()
+
+message=''
+text_box.pack(expand=True)
+text_box.insert('end', message)
+text_box.config(state='disabled')
 
 win.resizable(False, False)
 win.mainloop()
